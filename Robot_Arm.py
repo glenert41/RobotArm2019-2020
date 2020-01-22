fac1f182feb7aae8a433de323320fc44861920dd
Graham L Lenert <glenert41@ccs.local>
2020-01-22 12:33:58 -0500

    Getting Better

--- Robot_Arm.py
+++ Robot_Arm.py
@@ -26,31 +26,24 @@ shaftServo = servo.Servo(pwmShaftServo)
 
 shaftPot = AnalogIn(board.A1)
 
-//testing again
+
+pwmPitchServo = pulseio.PWMOut(board.A5, duty_cycle=2 ** 15, frequency = 50)
+pitchServo = servo.Servo(pwmPitchServo)
+
+pitchServoPot = AnalogIn(board.A0)
 
 
 while True:
  
-    mappedShaftPot = shaftPot.value/2**16*180; # simpleio.map_range(shaftPot,0,2^16,0,180)
+    mappedShaftPot = shaftPot.value/2**16*180 # simpleio.map_range(shaftPot,0,2^16,0,180)
     shaftServo.angle = int(mappedShaftPot)
     print(int(mappedShaftPot))
 
-
-    '''
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        shaftServo.angle = 1
-        print(shaftPot.value)
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        shaftServo.angle = 90
-        print(shaftPot.value)
-        time.sleep(0.05)
-    '''
+    mappedPitchPot = pitchPot.value/2**16*180
+    pitchServo.angle = int(pitchServoPot)
 
 
     time.sleep(.1)
     
 
    
-
-
