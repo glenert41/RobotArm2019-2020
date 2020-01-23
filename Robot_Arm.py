601240a95b59539c1097ce064867f02bb6c7f252
Graham L Lenert <glenert41@ccs.local>
2020-01-23 12:31:24 -0500

    2nd Servo Turns and Started DC Motor

--- Robot_Arm.py
+++ Robot_Arm.py
@@ -8,6 +8,7 @@ import analogio
 from analogio import AnalogIn
 import pulseio
 from adafruit_motor import servo
+from adafruit_motor import Motor
 import touchio
 import adafruit_bus_device
 
@@ -27,10 +28,15 @@ shaftServo = servo.Servo(pwmShaftServo)
 shaftPot = AnalogIn(board.A1)
 
 
-pwmPitchServo = pulseio.PWMOut(board.A5, duty_cycle=2 ** 15, frequency = 50)
+
+pwmPitchServo = pulseio.PWMOut(board.A3, duty_cycle=2 ** 15, frequency = 50)
 pitchServo = servo.Servo(pwmPitchServo)
 
 pitchServoPot = AnalogIn(board.A0)
+
+
+pwmMotor = pulseio.PWMOut(board.A2, frequency=50)
+my_Motor = servo.ContinuousServo(pwmMotor)
 
 
 while True:
@@ -38,9 +44,13 @@ while True:
     mappedShaftPot = shaftPot.value/2**16*180 # simpleio.map_range(shaftPot,0,2^16,0,180)
     shaftServo.angle = int(mappedShaftPot)
     print(int(mappedShaftPot))
-
-    mappedPitchPot = pitchPot.value/2**16*180
-    pitchServo.angle = int(pitchServoPot)
+ 
+    mappedPitchPot = pitchServoPot.value/2**16*180
+    pitchServo.angle = int(mappedPitchPot)
+    print(int(mappedPitchPot))
+    
+    my_Motor.throttle = 1.0
+    
 
 
     time.sleep(.1)
