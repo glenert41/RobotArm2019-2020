9dd0d97f6801c7965caf9c9e19a8e635c72e1a72
Graham L Lenert <glenert41@ccs.local>
2020-01-24 12:34:47 -0500

    2nd Dc Motor Code

--- Robot_Arm.py
+++ Robot_Arm.py
@@ -35,21 +35,30 @@ pitchServo = servo.Servo(pwmPitchServo)
 pitchServoPot = AnalogIn(board.A0)
 
 
+
+
 pwmMotor = pulseio.PWMOut(board.A2, frequency=50)
 my_Motor = servo.ContinuousServo(pwmMotor)
 
+motorPot = AnalogIn(board.A5)
 
 while True:
  
     mappedShaftPot = shaftPot.value/2**16*180 # simpleio.map_range(shaftPot,0,2^16,0,180)
     shaftServo.angle = int(mappedShaftPot)
-    print(int(mappedShaftPot))
+    #print(int(mappedShaftPot))
  
     mappedPitchPot = pitchServoPot.value/2**16*180
     pitchServo.angle = int(mappedPitchPot)
-    print(int(mappedPitchPot))
+   # print(int(mappedPitchPot))
+
+   
+
+   
     
-    my_Motor.throttle = 1.0
+    
+    pwmMotor.duty_cycle = motorPot.value
+    print(motorPot.value)
     
 
 
