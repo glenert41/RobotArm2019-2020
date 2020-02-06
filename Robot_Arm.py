f3c9380cd190cebc5b628a72b95f7ed6c0df4c32
Graham L Lenert <glenert41@ccs.local>
2020-02-06 12:31:04 -0500

    Printing RPS To LCD

--- Robot_Arm.py
+++ Robot_Arm.py
@@ -1,3 +1,7 @@
+#This code makes a thing spin really fast and
+# launch it in to the stratosphere
+#Like a freaking explosion of fast upward momentum. 
+#In theory its really cool, I promise
 import gc
 import board
 import math
@@ -22,20 +26,15 @@ import simpleio
 #pylint: disable-msg=import-error
 
 
-# Shaft Servo
-# Pitch Servo
 
-#Dc Motor
-
-#Photointerrupter
-
+#Shaft Servo Set Up
 pwmShaftServo = pulseio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50)
 shaftServo = servo.Servo(pwmShaftServo)
 
 shaftPot = AnalogIn(board.A1)
 
 
-
+#Pitch Servo Set Up
 pwmPitchServo = pulseio.PWMOut(board.A3, duty_cycle=2 ** 15, frequency = 50)
 pitchServo = servo.Servo(pwmPitchServo)
 
@@ -43,61 +42,46 @@ pitchServoPot = AnalogIn(board.A0)
 
 
 
-
+#Motor Set Up
 pwmMotor = pulseio.PWMOut(board.A2, frequency=50)
 my_Motor = servo.ContinuousServo(pwmMotor)
 
 motorPot = AnalogIn(board.A5)
 
 
-
+#PhotoInteruppter SetUp
 photoPin = digitalio.DigitalInOut(board.D8)
 photoPin.direction = digitalio.Direction.INPUT
 photoPin.pull = digitalio.Pull.UP
-
 interrupts = -1         
 lread = True
 fread = True 
 
-
+#LCD Set Up
 from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface #importing and prepping LCD library
-
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface 
 from lcd.lcd import CursorMode
-
 lcd = LCD(I2CPCF8574Interface(0x3F), num_rows=2, num_cols=16)
+
+
 
 initial = time.monotonic()
 
 while True:
 
     now = time.monotonic() 
-
-    mappedShaftPot = int(shaftPot.value/2**16*180) # simpleio.map_range(shaftPot,0,2^16,0,180)
+    
+    mappedShaftPot = int(shaftPot.value/2**16*180)
     shaftServo.angle = int(mappedShaftPot)
-    #print(int(mappedShaftPot))
  
     mappedPitchPot = int(pitchServoPot.value/2**16*180)
     pitchServo.angle = int(mappedPitchPot)
 
-
     MotorPotOut = motorPot.value
-    #print(MotorPotOut)
 
+# ----------------------------------------------------    
+   # Motor Potentiometer 
 
-    if photoPin.value:
-        lread = True   #if interrupted, turn lread to true
-    elif fread == lread: #if the photointerrupter is interrupted, then uniterrupted,                               #count the interrupter and add 1 to the counter variable
-        interrupts +=1   
-        lread = not lread
-    #print(interrupts)
-    lcd.set_cursor_pos(1,0)
-    lcd.print(("Interrupts: ") + str(interrupts))
-
-
-
-    
-    
     if(MotorPotOut <= 5000):
         pwmMotor.duty_cycle = 0
     elif(MotorPotOut >= 60000):
@@ -107,10 +91,23 @@ while True:
     
     displayMotorPotOut = int(simpleio.map_range(MotorPotOut,0,2**16-1,0,100))
     
+#-----------------------------------------------------
+
+    #Just here becauses it is paired with the RPS printing Below
+    if photoPin.value:
+        lread = True   
+    elif fread == lread:                             
+        interrupts +=1   
+        lread = not lread
+
+#LCD 1 Second Updates
+
     if now - initial >= 1:
         lcd.clear()
         lcd.set_cursor_pos(0,0)         #clears and resets LCD
         
+        
+
         if(MotorPotOut <= 5000):
             lcd.print(str("Speed: 0"))
         elif(MotorPotOut >= 60000):
@@ -118,12 +115,15 @@ while True:
         else:
             lcd.print(("Speed: ") + (str(displayMotorPotOut)))
 
+        #RPS Printing        
+        lcd.set_cursor_pos(1,0)
+        lcd.print(("RPS: ") + str(interrupts))
+        interrupts = 0
+
+        #End of the 1 Second Check
         initial = time.monotonic()
         
         
-    
-    
-
 
     time.sleep(.05)
     
